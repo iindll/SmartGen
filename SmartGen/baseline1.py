ce3c5c4bf685f72ce7ebd0b4d0807e822a886c5d
iindll <nadaabobaker11@gmail.com>
2026-06-26 11:00:35 +0300

    Reverted the baseline to its original version

--- SmartGen/baseline1.py
+++ SmartGen/baseline1.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import pickle
 import random
@@ -5,26 +6,14 @@ import random
 import numpy as np
 import torch
 import torch.nn as nn
-from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.metrics import accuracy_score, recall_score, precision_score, confusion_matrix
+from torch import optim
 from torch.utils.data import DataLoader
 
-from models1 import TransformerAutoencoder, TimeSeriesDataset1
+from models1 import TransformerAutoencoder, TimeSeriesDataset2, TimeSeriesDataset3, TimeSeriesDataset4
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
-
-def pad(vocab_size, sequences):
-    for sequence in sequences:
-        if len(sequence) < 40:
-            sequence.extend([vocab_size - 1] * (40 - len(sequence)))
-    return sequences
-
-
-def remove_pad(lst):
-    for sublist in lst:
-        while sublist and sublist[-1] == 0:
-            sublist.pop()
-    return lst
+vocab_dic = {"an": 141, "fr": 223, "us": 269, "sp": 235}
 
 
 def setup_seed(seed):
@@ -42,120 +31,247 @@ def setup_seed(seed):
     torch.backends.cudnn.deterministic = True
 
 
-def simi_pad(sequences):
-    for sequence in sequences:
-        if len(sequence) < 40:
-            sequence.extend([0] * (40 - len(sequence)))
+def pad(vocab_size, sequences):
+    for i in range(len(sequences)):
+        if len(sequences[i]) < 40:
+            sequences[i].extend([vocab_size - 1] * (40 - len(sequences[i])))
+        elif len(sequences[i]) > 40:
+            sequences[i] = sequences[i][:40]
     return sequences
 
 
-def make_data(vocab_size, data_file='reduced_flattened_useful_us_trn_instance_10.pkl', batch_size=64):
+def split_random(data_file, train_file, vld_file, split_ratio=0.8, seed=2024):
     with open(data_file, 'rb') as file:
-        sequence = pickle.load(file)
-    data = pad(vocab_size, sequence)
+        data = pickle.load(file)
+    random.seed(seed)
+    split_index = int(len(data) * split_ratio)
+    random.shuffle(data)
+    dataset_1 = data[:split_index]
+    dataset_2 = data[split_index:]
+    with open(train_file, 'wb') as f3:
+        pickle.dump(dataset_1, f3)
+    with open(vld_file, 'wb') as f3:
+        pickle.dump(dataset_2, f3)
+    return dataset_1, dataset_2
+
+
+def get_args_parser():
+    parser = argparse.ArgumentParser('MAE pre-training', add_help=False)
+    parser.add_argument('--epochs', default=15, type=int)
+    parser.add_argument('--model', default='TransformerAutoencoder', type=str, metavar='MODEL',
+                        help='Name of model to train: Autoencoder/GRUAutoencoder/TransformerAutoencoder/MaskedAutoencoder')
+    parser.add_argument('--dataset', default='fr', type=str, metavar='MODEL',
+                        help='Name of dataset to train: an/fr/us/sp')
+    parser.add_argument('--mask_strategy', default='top_k_loss', type=str, metavar='MODEL',
+                        help='Mask strategy:random/top_k_loss/loss_guided')
+    return parser
+
+
+def make_data(new_env, vocab_size, data_file='reduced_flattened_useful_us_trn_instance_10.pkl', batch_size=32):
+    with open(data_file, 'rb') as file:
+        sequences = pickle.load(file)
+    data = pad(vocab_size, sequences)
     data = np.array(data)
-    dataset = TimeSeriesDataset1(vocab_size, data)
+    if new_env == 'spring':
+        dataset = TimeSeriesDataset2(vocab_size, data)
+    elif new_env == 'night':
+        dataset = TimeSeriesDataset3(vocab_size, data)
+    elif new_env == 'multiple':
+        dataset = TimeSeriesDataset4(vocab_size, data)
+    else:
+        dataset = None
+
     data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
     return data_loader
 
 
-class TransformerAutoencoder(nn.Module):
-    def __init__(self, vocab_size, d_model=512, nhead=8, num_encoder_layers=2, num_decoder_layers=2):
-        super().__init__()
-        self.d_model = d_model
-        self.embedding = nn.Embedding(vocab_size, d_model)
+def train(new_env, vocab_size, epochs, train_file, model_name, seq_len):
+    model = TransformerAutoencoder(vocab_size=vocab_size, d_model=512, nhead=8, num_encoder_layers=2,
+                                   num_decoder_layers=2)
+    model = model.to(device)
+    criterion = nn.CrossEntropyLoss(reduction='none')
+    optimizer = optim.Adam(model.parameters(), lr=1e-3)
+    num_epochs = epochs
+    train_loader = make_data(new_env, vocab_size, data_file=train_file)
 
-        encoder_layer = nn.TransformerEncoderLayer(d_model, nhead, batch_first=True)
-        self.encoder = nn.TransformerEncoder(encoder_layer, num_encoder_layers)
-
-        decoder_layer = nn.TransformerDecoderLayer(d_model, nhead, batch_first=True)
-        self.decoder = nn.TransformerDecoder(decoder_layer, num_decoder_layers)
-
-        self.output_layer = nn.Linear(d_model, vocab_size)
-
-        self._init_weights()
-
-    def _init_weights(self):
-        for p in self.parameters():
-            if p.dim() > 1:
-                nn.init.xavier_uniform_(p)
-
-    def forward(self, src, src_key_padding_mask=None):
-        src_emb = self.embedding(src)
-        memory = self.encoder(src_emb, src_key_padding_mask=src_key_padding_mask)
-        tgt_emb = src_emb
-
-        return memory
-
-
-def SPPC_select(dataset, ori_env, vocab_size, threshold):
-    setup_seed(2024)
-    num_epochs = 15
-    for day in range(7):
-
-        model = TransformerAutoencoder(vocab_size, d_model=256, nhead=4, num_encoder_layers=2, num_decoder_layers=2)
-        model = model.to(device)
-        model_name = f"IoT_model/Transformer_{dataset}_{ori_env}_{num_epochs}epoch.pth"
-        model.load_state_dict(torch.load(model_name))
-        day_select_file = f'IoT_data/{dataset}/{ori_env}/trn_day_{day}.pkl'
-
-        with open(day_select_file, 'rb') as file2:
-            text_collection = pickle.load(file2)
-        print(len(text_collection))
-        train_loader = make_data(vocab_size, data_file=day_select_file, batch_size=len(text_collection))
-        criterion = nn.CrossEntropyLoss()
-        optimizer = torch.optim.Adam(model.parameters())
-
+    for epoch in range(num_epochs):
+        total_loss = 0
+        total_loss_all = 0
+        loss_vector = {}
+        number_vector = {}
         for batch in train_loader:
-            src, padding_mask, _ = batch
+            src, padding_mask, mask_v = batch
             src = src.to(device)
+            mask_v = mask_v.to(device)
             padding_mask = padding_mask.to(device)
-            memory = model(src, src_key_padding_mask=padding_mask)
-            memories = memory.cpu().detach().numpy()
-            memories_2D = memories.reshape(memories.shape[0], memories.shape[1] * memories.shape[2])
 
-        similarity_matrix = cosine_similarity(memories_2D)
+            output = model(src, src_key_padding_mask=padding_mask)
+            src = src.cuda().long()
 
-        to_remove = set()
-        unique_indices = []
+            loss = criterion(output.view(-1, vocab_size), src.view(-1))
+            loss = loss.reshape(-1, seq_len) * mask_v
+            loss = torch.sum(loss) / torch.sum(mask_v)
+            total_loss += loss.item()
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        torch.save(model.state_dict(), model_name)
+        avg_loss = total_loss / len(train_loader)
+        print(
+            f"Epoch [{epoch + 1}/{num_epochs}] - Train Loss: {avg_loss:.4f}")
 
-        for i in range(len(text_collection)):
-            if i not in to_remove:
-                unique_indices.append(i)
-                for j in range(i + 1, len(text_collection)):
-                    if similarity_matrix[i, j] > threshold:
-                        to_remove.add(j)
-
-        deduplicated_collection = [text_collection[i] for i in unique_indices]
-
-        print(deduplicated_collection)
-        print(len(deduplicated_collection))
-
-        with open(f'IoT_data/{dataset}/{ori_env}/trn_day_{day}_SPPC_th={threshold}.pkl', 'wb') as f3:
-            pickle.dump(deduplicated_collection, f3)
+    print('Finished Training')
 
 
-def similarity_select(dataset, ori_env, threshold):
-    for day in range(7):
-        with open(f'IoT_data/{dataset}/{ori_env}/trn_day_{day}.pkl', 'rb') as file3:
-            text_collection = pickle.load(file3)
+def find_threshold(new_env, vocab_size, vld_file, model_name, seq_len, percentage):
+    val_loader = make_data(new_env, vocab_size, data_file=vld_file, batch_size=1)
+    model = TransformerAutoencoder(vocab_size, d_model=512, nhead=8, num_encoder_layers=2, num_decoder_layers=2)
+    criterion = nn.CrossEntropyLoss(reduction='none')
+    model.load_state_dict(torch.load(model_name))
+    model.eval()
+    losses = []
+    model.to(device)
+    total_loss = 0
+    for batch in val_loader:
+        src, padding_mask, mask_v = batch
+        src = src.to(device)
+        mask_v = mask_v.to(device)
+        padding_mask = padding_mask.to(device)
 
-        simi_pad(text_collection)
-        similarity_matrix = cosine_similarity(text_collection)
-        remove_pad(text_collection)
-        to_remove = set()
-        unique_indices = []
+        output = model(src, src_key_padding_mask=padding_mask)
+        src = src.cuda().long()
 
-        for i in range(len(text_collection)):
-            if i not in to_remove:
-                unique_indices.append(i)
-                for j in range(i + 1, len(text_collection)):
-                    if similarity_matrix[i, j] > threshold:
-                        to_remove.add(j)
+        loss = criterion(output.view(-1, vocab_size), src.view(-1))
+        loss = loss.reshape(-1, seq_len) * mask_v
+        loss = torch.sum(loss) / torch.sum(mask_v)
+        losses.append(loss.item())
+        total_loss += loss.item()
+    avg_loss = total_loss / len(val_loader)
+    print(f"Avg Loss (Validation Dataset): {avg_loss:.4f}")
 
-        deduplicated_collection = [text_collection[i] for i in unique_indices]
-        print(deduplicated_collection)
-        print(len(deduplicated_collection))
+    threshold = np.percentile(losses, percentage)
+    print(f"Percentage:{percentage}% Threshold: {threshold}")
+    return threshold
 
-        with open(f'IoT_data/{dataset}/{ori_env}/trn_day_{day}_similarity_th={threshold}.pkl', 'wb') as f3:
-            pickle.dump(deduplicated_collection, f3)
+
+def evaluate(new_env, vocab_size, test_file1, test_file3, model_name, seq_len, threshold):
+    with open(test_file1, 'rb') as file:
+        input_train_attack1 = pickle.load(file)
+
+    input_train_attack = input_train_attack1
+
+    with open(test_file3, 'rb') as file:
+        tmp = pickle.load(file)
+        tmp = pad(vocab_size, tmp)
+        input_train_test = []
+        for item in tmp:
+            input_train_test.append((item, 0))
+    input_train_tuple = input_train_test + input_train_attack
+
+    input_train = [item[0] for item in input_train_tuple]
+    pad(vocab_size, input_train)
+    labels = [item[1] for item in input_train_tuple]
+    model = TransformerAutoencoder(vocab_size, d_model=512, nhead=8, num_encoder_layers=2, num_decoder_layers=2)
+    model.to(device)
+
+    input_train = np.array(input_train)
+    batch_size = 1
+    if new_env == 'spring':
+        test_dataset = TimeSeriesDataset2(vocab_size, input_train)
+    elif new_env == 'night':
+        test_dataset = TimeSeriesDataset3(vocab_size, input_train)
+    elif new_env == 'multiple':
+        test_dataset = TimeSeriesDataset4(vocab_size, input_train)
+    else:
+        test_dataset = None
+
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+    criterion = nn.CrossEntropyLoss(reduction='none')
+    model.load_state_dict(torch.load(model_name))
+    model.eval()
+    losses = []
+    predictions = []
+    FP_samples = []
+    total_loss = 0
+    for batch in test_loader:
+        src, padding_mask, mask_v = batch
+        src = src.to(device)
+        mask_v = mask_v.to(device)
+        padding_mask = padding_mask.to(device)
+
+        output = model(src, src_key_padding_mask=padding_mask)
+        src = src.cuda().long()
+
+        loss = criterion(output.view(-1, vocab_size), src.view(-1))
+        loss = loss.reshape(-1, seq_len) * mask_v
+        loss = torch.sum(loss) / torch.sum(mask_v)
+        losses.append(loss.item())
+        total_loss += loss.item()
+    avg_loss = total_loss / len(test_loader)
+    print(f"Avg Loss (Test Dataset): {avg_loss:.4f}")
+
+    for i in range(len(losses)):
+        if losses[i] < threshold:
+            predictions.append(0)
+        else:
+            predictions.append(1)
+
+    cm = confusion_matrix(y_true=labels, y_pred=predictions)
+
+    for q in range(len(labels)):
+        if labels[q] == 1 and predictions[q] == 0:
+            FP_samples.append(input_train[q])
+
+    TN, FP, FN, TP = cm.ravel()
+    FPR = FP / (FP + TN)
+    FNR = FN / (FN + TP)
+
+    recall = recall_score(y_pred=predictions, y_true=labels)
+    precision = precision_score(y_pred=predictions, y_true=labels)
+    accuracy = accuracy_score(y_pred=predictions, y_true=labels)
+    f1_score = 2 * (precision * recall) / (precision + recall) if (precision + recall) > 0 else 0
+
+    print("True Positive:", TP)
+    print("True Negative:", TN)
+    print("False Positive:", FP)
+    print("False Negative:", FN)
+    print("False Positive Rate:", FPR)
+    print("False Negative Rate:", FNR)
+    print("Recall:", recall)
+    print("Precision:", precision)
+    print("Accuracy:", accuracy)
+    print("F1 Score:", f1_score)
+    max_loss = max(losses)
+    min_loss = min(losses)
+    print(f"Max loss {max_loss:.4f} , Min loss: {min_loss:.4f}")
+    print('Finished Test')
+    return TP, TN, FP, FN, FPR, FNR, recall, precision, accuracy, f1_score
+
+
+def Anomaly_detection(dataset, new_env, thres, method, model, percentage):
+    model_name = f"check_model/best_{dataset}_{model}_{method}.pth"
+    vocab_size = vocab_dic[dataset]
+    epochs = 15
+    seq_len = 10
+    data_file = f'filter_data/{dataset}/{new_env}/{dataset}_{new_env}_generation_{method}_th={thres}_{model}_seq_filter_true.pkl'
+    if new_env == 'multiple':
+        vld_file = data_file
+        train_file = data_file
+    else:
+        train_file = f'IoT_data/{dataset}/{new_env}/trn.pkl'
+        vld_file = f"IoT_data/{dataset}/{new_env}/rs_vld.pkl"
+        split_random(data_file, train_file, vld_file)
+    if new_env == 'spring':
+        test_file1 = f"attack/{dataset}/labeled_{dataset}_spring_attack_heater.pkl"
+    elif new_env == 'night':
+        test_file1 = f"attack/{dataset}/labeled_{dataset}_night_attack_time.pkl"
+    elif new_env == 'multiple':
+        test_file1 = f"attack/{dataset}/labeled_{dataset}_multiple_attack_tv.pkl"
+
+    test_file3 = f"IoT_data/{dataset}/{new_env}/split_test.pkl"
+    setup_seed(2024)
+    train(new_env, vocab_size, epochs, train_file, model_name, seq_len)
+    threshold = find_threshold(new_env, vocab_size, vld_file, model_name, seq_len, percentage=percentage)
+    TP, TN, FP, FN, FPR, FNR, recall, precision, accuracy, f1_score = evaluate(new_env, vocab_size, test_file1,
+                                                                               test_file3, model_name, seq_len,
+                                                                               threshold=threshold)
